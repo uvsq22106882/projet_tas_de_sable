8bdf8ed7e8a878c72b5cbeaa4d49c05e32407062
Suyan SUBRAMANIAM <suyan.subramaniam@ens.uvsq.fr>
2022-02-08 12:19:31 +0100

    oof

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,1 +1,25 @@
-tkinter
+from tkinter import *
+
+
+sandpile_w= Tk()
+
+sandpile_w.title("Tas de sable")
+
+sandpile_w.geometry("600x600")
+
+
+monCanvas = Canvas(sandpile_w, width=500, height=500, bg='ivory')
+monCanvas.pack()
+monCanvas.place(x=50,y=50)
+
+def fun():  
+    messagebox.showinfo("Hello", "Red Button clicked")  
+
+
+b1 = Button(sandpile_w,text = "Red",command = fun,activeforeground = "red",activebackground = "pink",pady=10)  
+
+b1.pack(side = LEFT)  
+bouton = sandpile_w.Button (text = "zone de texte")
+
+sandpile_w.mainloop()
+
