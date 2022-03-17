5be78446827d9d688e031144d02feceb696bb184
Prénom NOM <prenom.nom@ens.uvsq.fr>
2022-03-17 19:12:36 +0100

    //

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -2,22 +2,31 @@ import tkinter as tk
 import random as rd 
 HEIGHT = 500
 WIDTH = 500
-l1=[]
-l2=[]
-l3=[]
-def configuration():
-    global l1,l2,l3
-    l1=[rd.randint(0,9),rd.randint(0,9),rd.randint(0,9)]
-    l2=[rd.randint(0,9),rd.randint(0,9),rd.randint(0,9)]
-    l3=[rd.randint(0,9),rd.randint(0,9),rd.randint(0,9)] 
-    print(l1)
-    print(l2)
-    print(l3)
-    return 
-#utiliser le random pour créer 3 listes aleatoires
+l=[[0,0,0],[0,0,0],[0,0,0]]
+liste=[]
+couleur = {0:"white", 1:"grey", 2:"purple", 3:"blue", 4:"green", 5:"yellow", 6:"orange", 7:"red"}
+#créer une fonction aleatoire pour avoir une grille 
+
+def configuration_aléatoire():
+    global l , liste
+    for i in range(len(l)):
+        for j in range (len(l)):
+            l[i][j]=rd.randint(0,9)
+    liste.append(l)
+    print(liste)
+
+
+def initialisation():
+  l=[[0,0,0],[0,0,0],[0,0,0]]
+  liste.append(l)
+  print(liste)     
+#utiliser le random pour créer une grille aleatoires
+###################################
 racine = tk.Tk() # Création de la fenêtre racine
 canvas = tk.Canvas(racine, bg="white", height=HEIGHT, width=WIDTH)
 canvas.grid()
-Bouton=tk.Button(racine, text="configuration aléatoire " , command=configuration , font=("Calibri","17"))
-Bouton.grid()
+Bouton1=tk.Button(racine, text="configuration aléatoire" , command= configuration_aléatoire , font=("Calibri","17"))
+Bouton2=tk.Button(racine, text="initialisation" , command=initialisation , font=("calibir","17"))
+Bouton1.grid()
+Bouton2.grid()
 racine.mainloop() # Lancement de la boucle principale
